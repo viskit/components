450174ba37946dcd197624fddaf0dd98540132fe
zale <lionzale@gmail.com>
2022-03-28 10:10:58 +0800

    update

--- src/components/vis-textarea.ts
+++ src/components/vis-textarea.ts
@@ -1,45 +1,145 @@
-import { LitElement, html, css, CSSResultGroup, TemplateResult } from "lit";
+import {
+  LitElement,
+  html,
+  css,
+  CSSResultGroup,
+  render,
+  TemplateResult,
+  CSSResult,
+  unsafeCSS,
+} from "lit";
 import { customElement, property, query, state } from "lit/decorators.js";
 import { repeat } from "lit/directives/repeat.js";
+import { guard } from "lit/directives/guard.js";
 
 @customElement("vis-textarea")
 export class Textarea extends LitElement {
   static styles = css`
-    .container {
-      outline: none;
+    iframe {
+      border: none;
+      outline: 0px;
+      min-height: 100vh;
     }
-    .container div {
-      font-size: var(--vis-font-size, 1rem);
-      min-height: var(--vis-font-size, 1rem);
+    :host {
+      contain: content;
+      width: 100%;
     }
   `;
 
-  @query(".container")
-  container: HTMLDivElement;
+  private get editor() {
+    return this.iframe.contentWindow.document.querySelector(
+      "#editor"
+    ) as HTMLElement;
+  }
+
+  @query("iframe")
+  private iframe: HTMLIFrameElement;
+
+  #loaded = false;
+
+  updated(map: Map<string, any>) {
+    if (map.has("value") && this.#loaded) {
+      render("", this.editor);
+      render(this.config.render(this.value), this.editor);
+    }
+  }
+
+  // input -------> dom
+  insert() {}
+
+  insertImage(src: string) {
+    this.editor.focus();
+    const selection = this.iframe.contentWindow.getSelection();
+    const range = selection.getRangeAt(0);
+    const img = new Image();
+    img.src = src;
+    range.insertNode(img);
+    const divEl = document.createElement("div");
+    img.parentElement.appendChild(divEl);
+    range.selectNode(divEl);
+  }
 
   @property({ type: String })
-  value = "";
+  value: string = "";
 
-  text2template(text: string) {
-    return text
+  #parse = (dom: HTMLElement) => {
+    const imgs = dom.querySelectorAll("img");
+    for (let i = 0; i < imgs.length; i++) {
+      const img = imgs.item(i);
+      img.insertAdjacentText("beforebegin", `![](${img.src})`);
+    }
+    const result = dom.innerText;
+    return result;
+  };
+
+  #render = (value: string) => {
+    const fragment = document.createDocumentFragment();
+
+    const divs = value
       .split("\n")
       .map(
-        (tr) => html`
-          <div>${tr.split(" ").map((v) => (v ? v : html`&nbsp;`))}</div>
-        `
+        (row) =>
+          `<div>${row.split(" ").map((k) => (k ? k : html`&nbsp;`))}</div>`
       );
+    fragment.textContent = `
+      <style>
+        .editor div:nth-of-type(1) {
+          color: blue;
+        }
+      </style>   
+      ${divs.join("")}     
+    `;
+
+    return fragment;
+  };
+
+  config: {
+    render: (text: string) => unknown;
+    parse: (dom: HTMLElement) => unknown;
+  } = {
+    render: this.#render,
+    parse: this.#parse,
+  };
+
+  getValue<T>() {
+    const clone = this.editor.cloneNode(true) as HTMLElement;
+    clone.style.opacity = "0";
+    this.editor.parentElement.insertAdjacentElement("beforeend", clone);
+    const result = this.#parse(clone);
+    clone.remove();
+    return result as unknown as T;
   }
 
   render() {
-    return html`
-      <div
-        class="container"
-        @focus=${() => console.log("focus")}
-        spellcheck="false"
-        contenteditable="true"
-      >
-        ${this.text2template(this.value)}
-      </div>
-    `;
+    return guard(
+      true,
+      () => html`
+        <iframe
+          @load=${() => {
+            this.#loaded = true;
+            this.requestUpdate("value");
+          }}
+          srcdoc=${`
+          <head>
+          <style>
+            html,body{
+                margin:0;padding:0;
+                min-height:100%;
+                width:100%;
+                --font-size: 1.2rem;
+                font-size: var(--font-size);
+            }
+            div{
+              min-height:var(--font-size);
+            }
+          </style>
+          </head>
+          <body >
+            <div id="editor" class="editor" spellcheck="false"  contentEditable="true"></div>
+          </body>
+          `}
+        ></iframe>
+      `
+    );
   }
 }
--- start/test-vis-textarea.js
+++ start/test-vis-textarea.js
@@ -1,11 +1,14 @@
 import "../src/components/vis-textarea";
+import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
 
+import url from "./love.jpg";
 document.body.insertAdjacentHTML(
   "afterbegin",
   `
     <h3> Test vis-textarea component : </h3>
     <button id="vis-textarea-test-button"> text2html </button>
-    <button id="vis-textarea-test-button2"> html2text </button>
+    <button id="vis-textarea-test-button2"> add image </button>
+    <button id="vis-textarea-test-button3"> get value </button>
     <vis-textarea></vis-textarea>
     
 `
@@ -15,11 +18,25 @@ const textarea = document.querySelector("vis-textarea");
 
 const btn = document.querySelector("#vis-textarea-test-button");
 const btn2 = document.querySelector("#vis-textarea-test-button2");
-btn2.onclick = () => {
-  console.log(textarea.container.innerText);
-}
+const btn3 = document.querySelector("#vis-textarea-test-button3");
+let num = 0;
 btn.onclick = () => {
-  textarea.value = `aaa \n \n \n bbbb \\<script\> 
-  alert(32332)>
-</script> \r\s\\s\\s  bbcc \n ddddd`;
+  textarea.value = "hello ...";
+  textarea.requestUpdate("value");
+};
+
+btn2.onclick = async () => {
+  const image = await Camera.getPhoto({
+    quality: 90,
+    allowEditing: true,
+    source: CameraSource.Camera,
+    resultType: CameraResultType.DataUrl,
+  });
+  var imageUrl = image.dataUrl;
+  textarea.insertImage(imageUrl);
+};
+
+btn3.onclick = async () => {
+  console.log(333);
+  console.log(textarea.getValue());
 };
